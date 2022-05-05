e22f8bce29671c15aaf140820edb1920aa9a4d5c
Sayam Gautam <sayamgautam1@gmail.com>
2022-05-05 21:12:44 +0930

    add basic questions and check the prompt working in command line

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "projectTitle",
+    message: "What is your project title? ",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "please add a brief description of your project",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt([...questions]).then((answers) => {
+    ///hshsh
+    console.log(answers);
+  });
+}
 
 // Function call to initialize app
 init();
